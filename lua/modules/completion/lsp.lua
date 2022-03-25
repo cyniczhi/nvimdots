a169de5c4f47fe9547876505551dcd9d648a85ff
zhizheng <zhizheng.0619@bytedance.com>
2022-03-25 15:35:37 +0800

    opt: disable auto fmt for cpp

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -66,9 +66,9 @@ local function custom_attach(client)
 
     if client.resolved_capabilities.document_formatting then
         vim.cmd([[augroup Format]])
-        vim.cmd([[autocmd! * <buffer>]])
+        -- vim.cmd([[autocmd! * <buffer>]])
         vim.cmd(
-            [[autocmd BufWritePost <buffer> lua require'modules.completion.formatting'.format()]])
+            [[autocmd! BufWritePost *.lua,*.json,*.yaml,*.ts,*.js lua require'modules.completion.formatting'.format()]])
         vim.cmd([[augroup END]])
     end
 end
